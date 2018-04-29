93fbd381ff818e4ec5c56dabeb6f70c43c0f39ed
Mei <meyrehay@gmail.com>
2018-04-29 14:20:22 -0700

    Login Model has been scaffolded and built out

--- BIT285_SpicyShrimps/BIT285_SpicyShrimps/Models/Student.cs
+++ BIT285_SpicyShrimps/BIT285_SpicyShrimps/Models/Student.cs
@@ -9,9 +9,18 @@ namespace BIT285_SpicyShrimps.Models
     public class Student
     {
         [Key]
+        [Required]
         public int StudentID { get; set; }
+        [Required]
         public string FirstName { get; set; }
-        public string LastName { get; set; }
-        public string Teacher { get; set; }
+        [Required]
+        public string LastInitial { get; set; }
+        public string Username { get { return FirstName + ". " + LastInitial; } }
+        public int TeacherID { get; set; }
+        [Required]
+        public string OneWordPassword { get; set; }
+        public int Level { get; set; }
+        /*Navigation Property for Teacher*/
+        public virtual Teacher Teacher { get; set; }
     }
 }
--- BIT285_SpicyShrimps/BIT285_SpicyShrimps/Models/Teacher.cs
+++ BIT285_SpicyShrimps/BIT285_SpicyShrimps/Models/Teacher.cs
@@ -9,16 +9,19 @@ namespace BIT285_SpicyShrimps.Models
     public class Teacher
     {
         [Key]
+        [Required]
         public int TeacherID { get; set; }
         public string FirstName { get; set; }
         [Required]
         public string LastName { get; set; }
+        [Required]
         public string Prefix { get; set; }
+        public string Username { get { return Prefix + ". " + LastName; } }
         [Required]
-        public string username { get; set; }
-        [Required]
-        public string password { get; set; }
-        public int classroomCode { get; set; }
-        public virtual Student 
+        public string Password { get; set; }
+        public string ClassroomCode { get; set; }
+        public string Level { get; set; }
+        /*Collection of Students*/
+        public virtual ICollection<Student> Students { get; set; }
     }
 }
