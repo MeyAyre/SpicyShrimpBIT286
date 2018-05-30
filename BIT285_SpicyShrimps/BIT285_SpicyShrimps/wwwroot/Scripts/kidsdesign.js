329117d2a8bb0a3139cdbefb7aa57bcf87da82a3
Mei <meyrehay@gmail.com>
2018-05-30 12:32:11 -0700

    Made the homepage

---
+++ BIT285_SpicyShrimps/BIT285_SpicyShrimps/wwwroot/Scripts/kidsdesign.js
@@ -0,0 +1,49 @@
+﻿//Gem moving code was adopted from https://codepen.io/jonathan/pen/wixFp by Jonathan Marzullo
+//Using GSAP
+
+$(document).ready(function () {
+
+    //Gem moving code
+    var totalElements = 0;
+    var elementAnimationDelay = 0;
+    var elementCountUp = 0;
+    //Easter Egg
+    var magicsound = "Sounds/fairy-wand.wav";
+
+    function elementMoveDown(elementItem) {
+        if (elementCountUp != totalElements) {
+            elementCountUp++;
+        } else {
+            elementAnimationDelay = 0; //no delay after initial run
+        }
+
+        TweenMax.to(elementItem, 1.3, {
+            delay: elementAnimationDelay,
+            y: "+=20px",
+            yoyo: true,
+            repeat: -1,
+            ease: Power2.easeOut//,
+            //onCompleteParams:[elementItem],
+            //onComplete: elementMoveUp
+        });
+    }
+    function initPhoneHovering() {
+        $(".gems > div[id]").each(function () {
+            totalElements++;
+            elementAnimationDelay += 0.4;
+
+            var targetElement = $("#element-" + totalElements);
+
+            //init animation
+            elementMoveDown(targetElement);
+        });
+    }
+
+    initPhoneHovering();
+
+    //Easter Eggs
+    $('.gems').hover(function () {
+        console.log("PLEASE");
+        $('#audio').html('<audio autoplay><source src= ' + magicsound + '></audio>');
+    });
+});
